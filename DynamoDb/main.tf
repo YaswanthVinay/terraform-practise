20e85fae37b43c8a999cf57e9ba9073dac178dd1
yaswanth vinay <yaswanthpedapatnam007@gmail.com>
2024-04-25 18:13:00 +0530

    code coverage

--- DynamoDb/main.tf
+++ DynamoDb/main.tf
@@ -5,20 +5,20 @@ terraform {
       version = "5.42.0"
     }
   }
-  #have used backend as s3 ,in your case make sure that you create bucket and store your state file there
-
-  backend "s3" {
-    bucket = "terraforms3bucket007"
-    key    = "taint-command/terraform.tfstate"
-    region = "ap-south-1"
-  }
 }
 
 provider "aws" {
   # Configuration options
   region = "ap-south-1"
 }
-
+#have used backend as s3 ,in your case make sure that you create bucket and store your state file there
+terraform {
+  backend "s3" {
+    bucket = "terraforms3bucket007"
+    key    = "taint-command/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
 
 #server-1
 resource "aws_instance" "taint-server-1" {
